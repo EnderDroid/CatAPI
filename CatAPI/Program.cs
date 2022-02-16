ec77e579d14d94e51481b292fffadce55af6728c
test <test@EDROID>
2022-02-16 11:41:54 +0700

    Add project files.

---
+++ CatAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace CatAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Press Enter for cat images!!!");
+                Console.ReadLine();
+                await CallingAPI();
+            }
+        }
+
+        static HttpClient client = new HttpClient();
+
+        static async Task CallingAPI()
+        {
+            var stringTask = client.GetStringAsync("https://api.thecatapi.com/v1/images/search");
+            List<RequestedData> catUrl = JsonConvert.DeserializeObject<List<RequestedData>>(await stringTask);
+            System.Diagnostics.Process.Start(catUrl[0].url);
+        }
+    }
+}
---
+++ CatAPI/RequestedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAPI
+{
+    class RequestedData
+    {
+        public string url { get; set; }
+    }
+}
